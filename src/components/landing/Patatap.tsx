c6f2d5436e88d127436bdcf51138ed3b8111b6d1
Rahul-Aitla <aitlarahul@gmail.com>
2026-07-18 19:35:32 +0530

    patatap mobile fix

--- src/components/landing/Patatap.tsx
+++ src/components/landing/Patatap.tsx
@@ -10,11 +10,11 @@ export default function Patatap() {
       <RepeatSeparator />
       <SectionHeading heading="Interactive Experience" />
       <Container>
-        <div className="px-5">
-          <p className="text-muted-foreground mb-6 text-sm">
+        <div className="md:px-5">
+          <p className="text-muted-foreground mb-6 px-5 text-sm md:px-0">
             i love interactive stuff so enjoy this!
           </p>
-          <div className="overflow-hidden rounded-xl border mb-6">
+          <div className="overflow-hidden border md:mb-12 mb-6 md:rounded-xl">
             <iframe
               src="https://patatap.com/"
               className="aspect-video w-full"
